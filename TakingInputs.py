fd8c5541d75e37c9814d8d9fc382abeeb9dc4105
Marshal Wang <marshal.wang@GCCYJQBEJ1WK.local>
2019-09-11 10:46:35 -0400

    taking input

---
+++ TakingInputs.py
@@ -0,0 +1,18 @@
+#comment
+#It is essentai to comment
+
+#Input
+# The input function returns the string the user enters
+# All inputs start as Strings
+# To change the type of variable you "cast it"
+name = input("Please input your name: ")
+a = input("Please input first number: ")
+a=int(a)
+b = input("Please input second number: ")
+b=int(b)
+#Process
+product=a*b
+#Output
+print("Hi, "+name)
+print("The product of "+str(a)+" and "+str(b)+" is "+str(product)+".")
+#Takes two numbers and multiplies them
