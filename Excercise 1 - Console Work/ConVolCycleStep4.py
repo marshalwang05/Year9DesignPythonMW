310dd8b26c417deb5eda4ae92166e91bb474b914
Marshal Wang <marshal.wang@GCCYJQBEJ1WK.local>
2019-09-19 14:13:03 -0400

    Excerise 1 - Console Work

--- Excercise 1 - Console Work/ConVolCycleStep4.py
+++ Excercise 1 - Console Work/ConVolCycleStep4.py
@@ -5,6 +5,8 @@ print("\n\t\t\tV = \u03C0\u00d7radius\u00b2\u00d7height")
 print("\n\tThis program will take as input the radius and height")
 print("\tand print the volume.")
 
+file = open("data.txt","a")
+
 name = input("\n\tWhat is your name: ")
 
 r=1
@@ -12,22 +14,29 @@ h=1
 
 while (r!=0 or h!=0):
 
-	r=input("\n\tInput radius (cm): ")
-	r=int(r)
-	h=input("\n\tInput height (cm): ")
-	h=int(h)
+	try:
+		r=input("\n\tInput radius (cm): ")
+		r=int(r)
+		h=input("\n\tInput height (cm): ")
+		h=int(h)
+	except:
+		print("\n\t\tEnter a numeric value")
+		h=-1
+		r=-1
 
 	if (r>=0 or h>=0):
 
+		V=(math.pi)*r*r*h
+		V=round(V,2)
 		print("\n\t\tHi "+name+"!")
 		print("\n\t\tIn a cylinder with:")
 		print("\n\t\tRadius = "+str(r))
 		print("\n\t\tHeight = "+str(h))
-
-		V=(math.pi)*r*r*h
-		V=round(V,2)
+		file.write(str(V)+"\n")
 
 		print("\n\tThe volume of the cylinder is ",V)
 	else:
 		print("\n\t\tYou have entered an invalid value")
+
 print("END PROGRAM")
+file.close()
